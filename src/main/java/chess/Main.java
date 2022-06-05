0528d4b8f35eca26884a2772957fc778d352e5d4
Zhang Yu <zyu1206@gmail.com>
2022-06-05 20:15:01 +0200

    Add Main.java

---
+++ src/main/java/chess/Main.java
@@ -0,0 +1,11 @@
+package chess;
+
+import chess.controls.ApplicationController;
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(ApplicationController.class);
+    }
+}
